18b6ff87bcd10754697db80a6a1db0db62d7a936
Davide Pagano <davide.pagano@cern.ch>
2021-02-22 10:43:35 +0100

    Licence update

--- EcoMug.h
+++ EcoMug.h
@@ -1,3 +1,23 @@
+////////////////////////////////////////////////////////////////////////////
+// EcoMug: Efficient COsmic MUon Generator                                //
+// Copyright (C) 2021 Davide Pagano <davide.pagano@unibs.it>              //
+// EcoMug is based on the following work:                                 //
+//                 arXiv:XXXX.XXXX                                        //
+//                                                                        //
+// This program is free software: you can redistribute it and/or modify   //
+// it under the terms of the GNU General Public License as published by   //
+// the Free Software Foundation, either version 3 of the License, or      //
+// (at your option) any later version.                                    //
+//                                                                        //
+// This program is distributed in the hope that it will be useful,        //
+// but WITHOUT ANY WARRANTY; without even the implied warranty of         //
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          //
+// GNU General Public License for more details.                           //
+//                                                                        //
+// You should have received a copy of the GNU General Public License      //
+// along with this program.  If not, see <https://www.gnu.org/licenses/>. //
+////////////////////////////////////////////////////////////////////////////
+
 #ifndef EcoMug_H
 #define EcoMug_H
 
