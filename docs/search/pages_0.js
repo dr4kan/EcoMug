4f5f23dbd03df4bb29efef9e406f7a51fe14321e
Davide Pagano <davide.pagano@cern.ch>
2021-04-16 14:16:51 +0200

    update documentation

--- docs/search/pages_0.js
+++ docs/search/pages_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['ecomug_3a_20efficient_20cosmic_20muon_20generator_81',['EcoMug: Efficient COsmic MUon Generator',['../md__r_e_a_d_m_e.html',1,'']]]
+  ['ecomug_3a_20efficient_20cosmic_20muon_20generator_81',['EcoMug: Efficient COsmic MUon Generator',['../index.html',1,'']]]
 ];
