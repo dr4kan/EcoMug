576e57b67b24b8ef3fe4c49e4e064ec865825689
Davide Pagano <davide.pagano@cern.ch>
2021-06-06 21:36:13 +0200

    update doc

--- docs/search/all_1.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['ecomug',['EcoMug',['../class_eco_mug.html',1,'']]],
-  ['emgeometry',['EMGeometry',['../class_eco_mug.html#ae62c575c33da2b5bbb00d06f323ea2ee',1,'EcoMug']]],
-  ['emrandom',['EMRandom',['../class_e_m_random.html',1,'']]]
-];
--- docs/search/classes_0.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['ecomug',['EcoMug',['../class_eco_mug.html',1,'']]],
-  ['emrandom',['EMRandom',['../class_e_m_random.html',1,'']]]
-];
