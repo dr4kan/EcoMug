63d0cd9953a591a373c9c36e804a18b28e96ede3
Davide Pagano <davide.pagano@cern.ch>
2021-06-19 21:00:01 +0200

    doc update

---
+++ docs/search/all_4.js
@@ -0,0 +1,22 @@
+var searchData=
+[
+  ['setcylindercenterposition_24',['SetCylinderCenterPosition',['../class_eco_mug.html#a28660bd2900e9c1544693cdc303dad63',1,'EcoMug']]],
+  ['setcylinderheight_25',['SetCylinderHeight',['../class_eco_mug.html#a121bc5cec350126accf7e8fc8556348c',1,'EcoMug']]],
+  ['setcylinderradius_26',['SetCylinderRadius',['../class_eco_mug.html#abe3203809acf52431743cb23186b9155',1,'EcoMug']]],
+  ['setgenerationmethod_27',['SetGenerationMethod',['../class_eco_mug.html#aa2736072a334406b0776ee04e14470cd',1,'EcoMug']]],
+  ['sethspherecenterposition_28',['SetHSphereCenterPosition',['../class_eco_mug.html#a5bc0937a24386e82dafd5c02c7bba6f1',1,'EcoMug']]],
+  ['sethsphereradius_29',['SetHSphereRadius',['../class_eco_mug.html#aca2dbc8cd76d93b01777adb8b0ca7150',1,'EcoMug']]],
+  ['setmaximummomentum_30',['SetMaximumMomentum',['../class_eco_mug.html#a26f8afa3b4151ec38f485cc2f221ec98',1,'EcoMug']]],
+  ['setmaximumphi_31',['SetMaximumPhi',['../class_eco_mug.html#ae08fed7109704139a79f43e8fd34d03d',1,'EcoMug']]],
+  ['setmaximumtheta_32',['SetMaximumTheta',['../class_eco_mug.html#a491714aebf2837eaa1cc2c2000a6b6b3',1,'EcoMug']]],
+  ['setminimummomentum_33',['SetMinimumMomentum',['../class_eco_mug.html#a0f936c82acbb7edb97b73baec8fd6efc',1,'EcoMug']]],
+  ['setminimumphi_34',['SetMinimumPhi',['../class_eco_mug.html#aa4dd4685e85c2f643db4aa8b4af9b9a5',1,'EcoMug']]],
+  ['setminimumtheta_35',['SetMinimumTheta',['../class_eco_mug.html#aa5c98a4e67cfcf5ffcd888ca5d8a5e4a',1,'EcoMug']]],
+  ['setseed_36',['SetSeed',['../class_eco_mug.html#a293886c9f509a91552d7f75f1b4620ef',1,'EcoMug']]],
+  ['setskycenterposition_37',['SetSkyCenterPosition',['../class_eco_mug.html#ad234ce82375c8d3d5aa28be0310da4b5',1,'EcoMug']]],
+  ['setskysize_38',['SetSkySize',['../class_eco_mug.html#ad23cc0e41993cbd670c0f0b8a0122fcf',1,'EcoMug']]],
+  ['setusecylinder_39',['SetUseCylinder',['../class_eco_mug.html#a65dc8860d11feae9f1a7595c202935e4',1,'EcoMug']]],
+  ['setusehsphere_40',['SetUseHSphere',['../class_eco_mug.html#a18a2c0aefa068ff1f6b9a98594593ea2',1,'EcoMug']]],
+  ['setusesky_41',['SetUseSky',['../class_eco_mug.html#abe09151809f401745553c1ab17df9a7e',1,'EcoMug']]],
+  ['sky_42',['Sky',['../class_eco_mug.html#ae62c575c33da2b5bbb00d06f323ea2eeafe9c74ece7f193928aefc1c019d5551c',1,'EcoMug']]]
+];
---
+++ docs/search/pages_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['ecomug_3a_20efficient_20cosmic_20muon_20generator_85',['EcoMug: Efficient COsmic MUon Generator',['../md__r_e_a_d_m_e.html',1,'']]]
+];
