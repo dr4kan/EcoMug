7fffe3299fae88f2105aa3f7eab8e9f6cef1254a
Davide Pagano <davide.pagano@cern.ch>
2021-08-02 18:44:19 +0200

    restore files

--- docs/search/all_3.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['hsphere_24',['HSphere',['../class_eco_mug.html#ae62c575c33da2b5bbb00d06f323ea2eea6a944f64bc65280070c96f36b9de440c',1,'EcoMug']]]
-];
--- docs/search/classes_0.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['ecomug_45',['EcoMug',['../class_eco_mug.html',1,'']]],
-  ['emmaximization_46',['EMMaximization',['../class_e_m_maximization.html',1,'']]],
-  ['emrandom_47',['EMRandom',['../class_e_m_random.html',1,'']]]
-];
