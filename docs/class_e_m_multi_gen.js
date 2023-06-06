4819eb2a4c730577e0d243e4d8d4e0438b61d04c
dr4kan <davide.pagano@cern.ch>
2023-06-06 15:26:49 +0200

    Version 2.0.0

---
+++ docs/class_e_m_multi_gen.js
@@ -0,0 +1,11 @@
+var class_e_m_multi_gen =
+[
+    [ "GetGenerationMomentum", "class_e_m_multi_gen.html#ad28902a98e0f1420aab3f41eb9f04626", null ],
+    [ "GetGenerationMomentum", "class_e_m_multi_gen.html#ae2e22f6fa64cd750eefe9e490a42c5da", null ],
+    [ "GetGenerationPhi", "class_e_m_multi_gen.html#ad2314610bd37b18f8fc169fedf66941d", null ],
+    [ "GetGenerationPosition", "class_e_m_multi_gen.html#adb43f6ae18161282408da327b4d4d01b", null ],
+    [ "GetGenerationTheta", "class_e_m_multi_gen.html#a9cd16ed25a528c41d7030c59467561ec", null ],
+    [ "GetPID", "class_e_m_multi_gen.html#a4fb0a3824132694f5f54061942e76a62", null ],
+    [ "SetBckPID", "class_e_m_multi_gen.html#a4814cf94cc1d79e7431deec3fd3bca0b", null ],
+    [ "SetBckWeights", "class_e_m_multi_gen.html#a4308b754aaa4be033d65d932306fb6e5", null ]
+];
---
+++ docs/index.js
@@ -0,0 +1,7 @@
+var index =
+[
+    [ "Basic Usage", "index.html#autotoc_md1", null ],
+    [ "More Advanced Usage", "index.html#autotoc_md5", null ],
+    [ "Rate and time estimation", "index.html#autotoc_md6", null ],
+    [ "Deal with background", "index.html#autotoc_md7", null ]
+];
